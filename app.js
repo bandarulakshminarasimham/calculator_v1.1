7c0b915651eb6dd1dda165f1ff521cd77342f614
Lakshmi Narasimham Bandaru <lakshmi.narasimham@valuelabs.com>
2018-09-29 20:13:25 +0530

    git init

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	// Send the response body as "Hello World"
+	res.end('Hello World\n');
+}).listen(5000)
+console.log('application running on PORT: 5000 ')
